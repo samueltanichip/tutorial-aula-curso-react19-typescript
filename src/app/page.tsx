a53fbec66e45b90fb69dae4946c9f38c4fdf90af
Cesar <cesar@celke.com.br>
2025-01-16 10:35:19 -0300

    Como usar props e children no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
+import User from "@/components/User";
 
 const Home = () => {
+
+  const userName = "Cesar";
+
   return (
     <div>
       <Menu /><br />
+
+      <User name={userName}>
+        <p>Este é um conteúdo extra fornecido como children.</p>
+      </User><br />
+
       <h2>Bem-vindo Celke!</h2><br />
       <Footer />
     </div>
