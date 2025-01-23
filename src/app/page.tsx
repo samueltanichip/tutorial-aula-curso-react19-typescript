34528729c046292f5898fc6590624bcc38c540c3
Cesar <cesar@celke.com.br>
2025-01-23 10:49:27 -0300

    #5 - Como usar Hooks useState no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
+'use client'
+import { useState } from "react";
 import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
 import User from "@/components/User";
 
 const Home = () => {
 
-  const userName = "Cesar";
+  const [nameUser, setNameUser] = useState("Cesar");
 
   return (
     <div>
       <Menu /><br />
 
-      <User name={userName}>
+      <User name={nameUser}>
         <p>Este é um conteúdo extra fornecido como children.</p>
       </User><br />
+
+      <button onClick={() => setNameUser("Kelly")}>Alterar</button>
 
       <h2>Bem-vindo Celke!</h2><br />
       <Footer />
