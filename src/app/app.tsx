0c211769f47352b7a2086084c872b7e0c7f398b3
samueltanichip <samueltani@chiptronic.com.br>
2025-04-02 13:54:24 -0300

    Delete src directory

--- src/app/app.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return <h1>Meu App React</h1>;
-};
-
-export default App;
--- src/components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return(
-        <footer>
-            <p>&copy; { new Date().getFullYear() } Celke. Todos os direitos reservados</p>
-        </footer>
-    )
-}
-
-export default Footer;
